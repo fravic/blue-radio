57fd00cdd68ea0456acfb50da7e16f314499f2a4
iradicator <iradicator@gmail.com>
2018-01-26 20:36:05 -0800

    Adding basic click to move behavior

---
+++ BlueRadio/Assets/ClickToMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MoveToClickPoint.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 10000))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}

450fa893784b363e3215e14badc4d5adfc860b02
iradicator <iradicator@gmail.com>
2018-01-26 20:49:08 -0800

    Adding basic camera control

---
+++ BlueRadio/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    [SerializeField] private float scrollingSpeed;
+
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        transform.position += scrollingSpeed * new Vector3(x, 0.0f, z);
+    }
+}

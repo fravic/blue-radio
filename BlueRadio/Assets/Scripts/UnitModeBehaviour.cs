4968d622aef9eec8887e69360ffab32d093818e0
DESKTOP-5GKGLEP\Mark <shlukiman@gmail.com>
2018-01-27 16:46:36 -0800

    Adding connecting units to the motherbase

--- BlueRadio/Assets/Scripts/UnitModeBehaviour.cs
+++ BlueRadio/Assets/Scripts/UnitModeBehaviour.cs
@@ -4,47 +4,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class UnitModeBehaviour : MonoBehaviour {
+public class UnitModeBehaviour : MonoBehaviour
+{
 
     //public SphereCollider RadioSphereCollider;
+    public bool _isConnectedToMotherbase;
+    public bool IsConnectedToMotherbase { get { return _isConnectedToMotherbase; }
+      set { _isConnectedToMotherbase = value;
+            ConnectedIndicator.SetActive(value);
+        } }
 
-  public enum UnitMode
-  {
-    Van,
-    Tower,
-  };
+    public GameObject ConnectedIndicator;
 
-  public UnitMode currentMode;
-  public GameObject towerObj;
-  public GameObject vanObj;
+    public enum UnitMode
+    {
+        Van,
+        Tower,
+    };
 
-  public void Start()
-  {
-    currentMode = UnitMode.Van;
-  }
+    public UnitMode currentMode;
+    public GameObject towerObj;
+    public GameObject vanObj;
 
-  // Changes the unit mode from Tower to Van, or vice versa
-      public void Toggle()
-      {
+    public void Start()
+    {
+        currentMode = UnitMode.Van;
+        IsConnectedToMotherbase = false;
+    }
+
+    // Changes the unit mode from Tower to Van, or vice versa
+    public void Toggle()
+    {
         if (currentMode == UnitMode.Van)
         {
-          currentMode = UnitMode.Tower;
-          vanObj.GetComponent<Animator>().SetBool("isShrunk", true);
-          towerObj.GetComponent<Animator>().SetBool("isShrunk", false);
+            currentMode = UnitMode.Tower;
+            vanObj.GetComponent<Animator>().SetBool("isShrunk", true);
+            towerObj.GetComponent<Animator>().SetBool("isShrunk", false);
 
-          GetComponent<NavMeshAgent>().enabled = false;
-          GetComponent<ClickToMove>().StopMovement();
+            GetComponent<NavMeshAgent>().enabled = false;
+            GetComponent<ClickToMove>().StopMovement();
         }
         else
         {
-          currentMode = UnitMode.Van;
-          vanObj.GetComponent<Animator>().SetBool("isShrunk", false);
-          towerObj.GetComponent<Animator>().SetBool("isShrunk", true);
+            currentMode = UnitMode.Van;
+            vanObj.GetComponent<Animator>().SetBool("isShrunk", false);
+            towerObj.GetComponent<Animator>().SetBool("isShrunk", true);
 
-          GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<NavMeshAgent>().enabled = true;
         }
-      }
-
-
+    }
 
 }
---
+++ BlueRadio/Assets/SphereCollisionDetector.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCollisionDetector : MonoBehaviour {
+
+    private UnitModeBehaviour unit;
+
+	// Use this for initialization
+	void Start ()
+    {
+        unit = transform.parent.parent.GetComponent<UnitModeBehaviour>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger enter: " + other.gameObject.tag);
+        if (other.gameObject.tag == "PlayerMotherbase")
+            unit.IsConnectedToMotherbase = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerMotherbase")
+            unit.IsConnectedToMotherbase = false;
+
+        if (unit.IsConnectedToMotherbase && other.gameObject.tag == "PlayerSphere")
+        {
+            unit.IsConnectedToMotherbase = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (!unit.IsConnectedToMotherbase)
+        {
+            if (other.gameObject.tag == "PlayerSphere")
+            {
+                if (other.transform.parent.parent.GetComponent<UnitModeBehaviour>().IsConnectedToMotherbase)
+                    unit.IsConnectedToMotherbase = true;
+            }
+        }
+
+        if (other.gameObject.tag == "PlayerMotherbase")
+            unit.IsConnectedToMotherbase = true;
+    }
+}

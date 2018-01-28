1b8362345704eed4635aae24729ed59bd24a818d
iradicator <iradicator@gmail.com>
2018-01-27 16:00:29 -0800

    Adding an agressive unit

--- BlueRadio/Assets/Scripts/UnitModeBehaviour.cs
+++ BlueRadio/Assets/Scripts/UnitModeBehaviour.cs
@@ -8,43 +8,41 @@ public class UnitModeBehaviour : MonoBehaviour {
 
     //public SphereCollider RadioSphereCollider;
 
-  public enum UnitMode
-  {
-    Van,
-    Tower,
-  };
+    public enum UnitMode
+    {
+        Van,
+        Tower,
+        Tank,
+    };
 
-  public UnitMode currentMode;
-  public GameObject towerObj;
-  public GameObject vanObj;
+    public UnitMode currentMode;
+    public GameObject towerObj;
+    public GameObject vanObj;
 
-  public void Start()
-  {
-    currentMode = UnitMode.Van;
-  }
+    // Changes the unit mode from Tower to Van, or vice versa
+    public void Toggle()
+    {
+        if (currentMode == UnitMode.Tank)
+        {
+            return;
+        }
 
-  // Changes the unit mode from Tower to Van, or vice versa
-      public void Toggle()
-      {
         if (currentMode == UnitMode.Van)
         {
-          currentMode = UnitMode.Tower;
-          vanObj.GetComponent<Animator>().SetBool("isShrunk", true);
-          towerObj.GetComponent<Animator>().SetBool("isShrunk", false);
+            currentMode = UnitMode.Tower;
+            vanObj.GetComponent<Animator>().SetBool("isShrunk", true);
+            towerObj.GetComponent<Animator>().SetBool("isShrunk", false);
 
-          GetComponent<NavMeshAgent>().enabled = false;
-          GetComponent<ClickToMove>().StopMovement();
+            GetComponent<NavMeshAgent>().enabled = false;
+            GetComponent<ClickToMove>().StopMovement();
         }
         else
         {
-          currentMode = UnitMode.Van;
-          vanObj.GetComponent<Animator>().SetBool("isShrunk", false);
-          towerObj.GetComponent<Animator>().SetBool("isShrunk", true);
+            currentMode = UnitMode.Van;
+            vanObj.GetComponent<Animator>().SetBool("isShrunk", false);
+            towerObj.GetComponent<Animator>().SetBool("isShrunk", true);
 
-          GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<NavMeshAgent>().enabled = true;
         }
-      }
-
-
-
+    }
 }

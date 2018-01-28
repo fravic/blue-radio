7a83c9b8b738c4141e55281ed6d32b8bf23e27d7
iradicator <iradicator@gmail.com>
2018-01-27 16:46:43 -0800

    Adding ability to destroy on impact for agressive units

---
+++ BlueRadio/Assets/Scripts/DestroyOnImpact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnImpact : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(name + " Collided with " + collision.gameObject.name);
+        /// TODO: add a check for other player
+        if (collision.collider.tag == "Unit")
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
